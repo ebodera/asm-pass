eff1797a38d841d1171870960f23562d438780e8
imgurbot12 <imgurbot12@gmail.com>
2019-08-21 23:10:59 +0000

    trying to fix heroku

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from japronto import Application
 
 #** Variables **#
 is_heroku = 'ON_HIROKU' in os.environ
-web_port  = os.environ['PORT'] if is_heroku else 8000
+web_port  = int(os.environ.get('PORT', 8000)) if is_heroku else 8000
 
 # config = [
 #     # user api pages
